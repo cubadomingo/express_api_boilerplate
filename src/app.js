6f358ee5cbd2d4c9b3638c2d2b49d6af059fe352
Devin Osorio <me@devinosor.io>
2017-06-23 16:34:06 -0700

    adds app and server configuration files

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authController from './controllers/auth';
+
+dotenv.config();
+const app = express();
+const namespace = '/api/v1';
+
+if (process.env.NODE_ENV !== 'test') { app.use(morgan('dev')); }
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(`${namespace}/authenticate`, authController);
+app.use('*',function(req,res) {
+  res.status(404).json({
+    message: 'route does not exist'
+  });
+});
+
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message.split(',').join(', '),
+        error: err.status
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message.split(',').join(', '),
+        error: err
+    });
+});
+
+export default app;
