d0720ff5b7989654a515ec0b4f63a5f9561a771d
Devin Osorio <me@devinosor.io>
2017-06-23 19:50:34 -0700

    adds users endpoints with specs

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import userController from './controllers/user';
 import authController from './controllers/auth';
 
 dotenv.config();
 const app = express();
+
+// namespace for api versioning
 const namespace = '/api/v1';
 
 if (process.env.NODE_ENV !== 'test') { app.use(morgan('dev')); }
@@ -14,12 +17,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Routes
+
 app.use(`${namespace}/authenticate`, authController);
+app.use(`${namespace}/users`, userController);
 app.use('*',function(req,res) {
   res.status(404).json({
     message: 'route does not exist'
   });
 });
+
+// Error handling
 
 if (app.get('env') === 'production') {
   app.use(function(err, req, res, next) {
